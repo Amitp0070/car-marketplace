437001bf458677eb0fe0e9498db7f3b9978150ac
AmitPathak <amit7081483564@gmail.com>
2024-09-21 23:37:13 +0530

    Complete Front End Home Page

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,10 +7,10 @@ function Category() {
       <h2 className="font-bold text-center text-3xl mb-6">Browse By Types</h2>
       <div
         className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-9
-      hover:shadow-lg cursor-pointer lg:grid-cols-9 gap-6 px-20"
+        cursor-pointer lg:grid-cols-9 gap-6 px-20"
       >
         {Data.Category.map((category, index) => (
-          <div className="border rounded-lg p-3 items-center flex flex-col ">
+          <div className="border rounded-lg hover:shadow-md p-3 items-center flex flex-col ">
             <img src={category.icon} alt="" width={35} height={35} />
             <h2 className="mt-2">{category.name}</h2>
           </div>
