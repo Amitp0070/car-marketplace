5ec4fed4235b26f1238029ab8b7d2011c6be395c
AmitPathak <amit7081483564@gmail.com>
2024-09-28 14:02:32 +0530

    To do Latter

--- src/assets/profile/components/MyListing.jsx
+++ src/assets/profile/components/MyListing.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { db } from "./../../../../configs";
 import { CarImages, CarListing } from "./../../../../configs/schema";
 import { desc, eq } from "drizzle-orm";
 import { useUser } from "@clerk/clerk-react";
+import CarItem from "@/components/CarItem";
+import Service from "@/Shared/Service";
+
 
 function MyListing() {
 
 
   const { user } = useUser();
+  const [carList, setCarList]=useState([]);
 
   useEffect(() => {
     user&&GetUserCarListing();
@@ -21,7 +25,10 @@ function MyListing() {
       .leftJoin(CarImages, eq(CarListing.id, CarImages.carListingId))
       .where(eq(CarListing.createBy, user?.primaryEmailAddress?.emailAddress))
       .orderBy(desc(CarListing.id))
-    console.log(result);
+
+      const resp=Service.FormatResult(result)
+      console.log(resp);
+      setCarList(resp);
   };
 
   return (
@@ -31,6 +38,13 @@ function MyListing() {
         <Link to={"/add-listing"}>
           <Button>+ Add New Listing</Button>
         </Link>
+      </div>
+      <div>
+        {carList.map((item,index)=>(
+          <div key={index}>
+            <CarItem car={item} />
+          </div>
+        ))}
       </div>
     </div>
   );
